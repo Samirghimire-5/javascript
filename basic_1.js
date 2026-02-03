6913fe5c800305bb54680c5d37d2e58de0733e66
Samirghimire-5 <samirghimire9841@gmail.com>
2026-02-03 20:31:37 +0545

    basic js problem

---
+++ basic_1.js
@@ -0,0 +1,7 @@
+let a = 80;
+if (a > 100) console.log("invalid input");
+if (a >= 80 && a <= 100) console.log("distinction");
+if (a >= 70 && a < 80) console.log("first division");
+if (a >= 60 && a < 70) console.log("second division");
+if (a >= 50 && a < 60) console.log("third division");
+if (a < 50) console.log("fail");
