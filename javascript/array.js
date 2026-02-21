1018daba45df8db9c4388774a60e22d878f02522
Samirghimire-5 <samirghimire9841@gmail.com>
2026-02-21 16:58:31 +0545

    ternary and array

---
+++ javascript/array.js
@@ -0,0 +1,13 @@
+const subjects = ["htmlCssJs", "database", "python", "nepal"];
+
+let joined = subjects.join("😀");
+console.log(`I read ${subjects[1]}`)
+console.log(`I read ${subjects[0]}`)
+console.log(`I read ${subjects[2]}`)
+console.log(`I read ${subjects[3]}`)
+
+console.log(joined)
+
+// for (let i = 0; i < subjects.length; i += 1) {
+//   console.log("kun sub", subjects[i], "😀");
+// }
